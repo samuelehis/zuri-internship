ded1c4d7d69fa8bf98b08db1b7334b0780acf13d
samuelehis <samuel.ehis@outlook.com>
2022-06-23 10:01:42 +0100

    Introduction to Javascript

---
+++ jsintro.js
@@ -0,0 +1,5 @@
+const myName = 'Samuel';
+const myHeight = '15.6';
+const myCountry = 'Nigeria';
+
+alert("My name is " + myName + "\nI am " + myHeight + "m tall. \nI hail from " + myCountry +"." );
